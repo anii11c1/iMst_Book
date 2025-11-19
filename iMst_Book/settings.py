a41a4bdf636a42cad736b22652d667983cc93906
Anii <maranahi011@gmail.com>
2025-11-19 19:59:52 -0300

    Agrego ajustes en settings.py

--- iMst_Book/settings.py
+++ iMst_Book/settings.py
@@ -82,8 +82,7 @@ WSGI_APPLICATION = 'iMst_Book.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
+        conn_max_age=600
     )
 }
 
