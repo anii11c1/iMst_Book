a75dfb48fa8e32bcb6e0785281cd45730b1723bd
Anii <maranahi011@gmail.com>
2025-11-19 15:34:37 -0300

    Agrego render.yaml y ajustes en settings.py

--- iMst_Book/settings.py
+++ iMst_Book/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rc1#$lq#1y$@)lvw25iv+@56z*c3rjdesyh1(@x(l4gy2xn^e1'
+SECRET_KEY = "insecure-key-for-dev"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -128,9 +128,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join (BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join (BASE_DIR, "media")
-ALLOWED_HOSTS = ["*"]
+MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAuto.Field'
